56c26ee4d52f01a8e3ee4a82f1787573f5623998
Lovro Zelić <lzelic@bytecode.hr>
2026-05-06 14:23:15 +0200

    update hello_controller.js

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -1,7 +1,18 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  connect() {
-    this.element.textContent = "Hello World!"
+  static targets = ["name", "output"]
+
+  // connect() {
+  //   this.element.textContent = "Hello World!"
+  // }
+
+  greet() {
+    const name = this.nameTarget.value.trim()
+    if (name) {
+      this.outputTarget.textContent = `Hello, ${name}! Stimulus is working.`
+    } else {
+      this.outputTarget.textContent = "Please enter a name first!"
+    }
   }
 }
