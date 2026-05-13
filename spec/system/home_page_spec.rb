352b7c970ab27f8efffecebded217fcbc1e8d78f
Lovro Zelić <60779407+lz13@users.noreply.github.com>
2026-05-13 10:53:49 +0200

    Fix/GitHub ci (#18)
    * update github/ci
    
    * use rspec system tests instead of a minitest /test suite
    
    * add home_page system test
    
    * add support/capybara.rb and update rails_helper
    
    * update COMMANDS.rb
    
    * fixes
    
    * more fixes
    
    * build tailwind:css in CI tests
    
    * update capybara.rb

---
+++ spec/system/home_page_spec.rb
@@ -0,0 +1,28 @@
+require "rails_helper"
+
+RSpec.describe "Home Page", type: :system do
+  it "displays the hero section" do
+    visit root_path
+
+    expect(page).to have_content("Darts Tournament")
+    expect(page).to have_content("Manager")
+    expect(page).to have_link("Create Tournament")
+  end
+
+  it "navigates to tournament creation page" do
+    visit root_path
+    click_link "Create Tournament", match: :first
+
+    expect(page).to have_content("Create Tournament")
+    expect(page).to have_current_path(new_tournament_path)
+  end
+
+  it "stimulus controller responds to user interaction", js: true do
+    visit root_path
+
+    find('[data-hello-target="name"]').set("World")
+    click_button "Test Stimulus"
+
+    expect(page).to have_content("Hello, World! Stimulus is working.")
+  end
+end
