2d156cacca8c9bcd69084d8d436a236e96e9a514
Lovro Zelić <lzelic@bytecode.hr>
2026-05-13 10:36:59 +0200

    more fixes

--- spec/system/home_page_spec.rb
+++ spec/system/home_page_spec.rb
@@ -20,7 +20,7 @@ RSpec.describe "Home Page", type: :system do
   it "stimulus controller responds to user interaction", js: true do
     visit root_path
 
-    find("input.rounded-lg", match: :first).set("World")
+    find('[data-hello-target="name"]').set("World")
     click_button "Test Stimulus"
 
     expect(page).to have_content("Hello, World! Stimulus is working.")
