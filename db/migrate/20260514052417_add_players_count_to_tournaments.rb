c13f189811cae96bdd1b22a67fd33ecfa0e06bba
Lovro Zelić <lzelic@bytecode.hr>
2026-05-14 08:39:13 +0200

    update players_count migration

--- db/migrate/20260514052417_add_players_count_to_tournaments.rb
+++ db/migrate/20260514052417_add_players_count_to_tournaments.rb
@@ -2,12 +2,22 @@ class AddPlayersCountToTournaments < ActiveRecord::Migration[8.1]
   def change
     add_column :tournaments, :players_count, :integer, default: 0, null: false
 
-    # Backfill existing data
+    # Backfill existing data using raw SQL
     reversible do |dir|
       dir.up do
-        Tournament.find_each do |tournament|
-          Tournament.reset_counters(tournament.id, :players)
-        end
+        execute <<-SQL.squish
+          UPDATE tournaments
+          SET players_count = (
+              SELECT COUNT(*)
+              FROM players
+              WHERE players.tournament_id = tournaments.id
+          )
+          WHERE EXISTS (
+              SELECT 1
+              FROM players
+              WHERE players.tournament_id = tournaments.id
+          )
+        SQL
       end
     end
   end
