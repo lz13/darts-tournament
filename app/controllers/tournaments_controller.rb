9323722a2058956eef2b82a1f90e268ab45b7eb2
Lovro Zelić <60779407+lz13@users.noreply.github.com>
2026-05-15 08:06:20 +0200

    Feature/tournament management (#21)
    * add players controller
    
    * add tournaments controller
    
    * add views
    
    * update routes
    
    * add requests specs
    
    * update CHECKLIST.md
    
    * add player_count to tournaments
    
    * Create admin_token_verifiable
    
    * fixes
    
    * crearte clipboard_controller.je
    
    * udpdate helper specs
    
    * update tournaments_controller
    
    * update players_count migration
    
    * fixes
    
    * rubocop fixes
    
    * update tournaments_controller#start!
    
    * fixes
    
    * update players controller

--- app/controllers/tournaments_controller.rb
+++ app/controllers/tournaments_controller.rb
@@ -1,10 +1,15 @@
 class TournamentsController < ApplicationController
+  include AdminTokenVerifiable
+
+  before_action :set_tournament_by_share_token, only: %i[show]
+  before_action :set_tournament_by_share_token_and_verify_admin, only: %i[admin start]
+
   def index
-    # Placeholder
+    @tournaments = Tournament.order(created_at: :desc)
   end
 
   def new
-    # Placeholder
+    @tournament = Tournament.new
   end
 
   def show
@@ -12,7 +17,13 @@ class TournamentsController < ApplicationController
   end
 
   def create
-    # Placeholder
+    @tournament = Tournament.new(tournament_params)
+
+    if @tournament.save
+      redirect_to public_tournament_path(@tournament.share_token), notice: "Tournament created successfully!"
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def update
@@ -29,5 +40,33 @@ class TournamentsController < ApplicationController
 
   def admin
     # Placeholder
+  end
+
+  def start
+    manual_seeds_auto_assigned = @tournament.seeding_method == "manual" && @tournament.players.any? { |p| p.seed_number.blank? }
+
+    if @tournament.start!
+      notice_message = "Tournament started! Bracket will be generated in the next phase."
+      notice_message += " Note: Manual seeding was selected but no seeds were assigned. Players were seeded in order." if manual_seeds_auto_assigned
+
+      redirect_to admin_tournament_path(@tournament.share_token, @tournament.admin_token), notice: notice_message
+    else
+      redirect_to admin_tournament_path(@tournament.share_token, @tournament.admin_token), alert: "Failed to start tournament!"
+    end
+  end
+
+  private
+
+  def tournament_params
+    params.require(:tournament).permit(:name, :legs_to_win, :seeding_method)
+  end
+
+  def set_tournament_by_share_token
+    @tournament = Tournament.find_by!(share_token: params[:share_token])
+  end
+
+  def set_tournament_by_share_token_and_verify_admin
+    @tournament = Tournament.find_by!(share_token: params[:share_token])
+    raise ActiveRecord::RecordNotFound unless valid_admin_token?(params[:admin_token])
   end
 end
--- app/models/player.rb
+++ app/models/player.rb
@@ -1,5 +1,5 @@
 class Player < ApplicationRecord
-  belongs_to :tournament
+  belongs_to :tournament, counter_cache: true
 
   validates :name, presence: true
   validates :seed_number, numericality: { only_integer: true, greater_than: 0 }, allow_nil: true
